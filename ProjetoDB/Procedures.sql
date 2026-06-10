fec3e29e82c0e94179a29dc44f96b02902ac9285
csrッ <99185429+CesarZanelatto@users.noreply.github.com>
2026-06-10 11:17:50 -0300

    feat- adicionado a procedure(insert notebook) Rafa

--- ProjetoDB/Procedures.sql
+++ ProjetoDB/Procedures.sql
@@ -32,3 +32,50 @@ BEGIN
 END//
  
 DELIMITER ;
+
+--INSERT NOTEBOOK
+ 
+DELIMITER //
+ 
+CREATE PROCEDURE inserirNotebook(
+    IN patrimonio_p VARCHAR(50),
+    IN marca_p VARCHAR(50),
+    IN modelo_p VARCHAR(100),
+    IN numero_serie_p VARCHAR(100),
+    IN processador_p VARCHAR(100),
+    IN memoria_p VARCHAR(20),
+    IN armazenamento_p VARCHAR(20),
+    IN data_p DATE,
+    IN status_p VARCHAR(20),
+    IN id_funcionario_p INT
+)
+BEGIN
+ 
+    INSERT INTO notebook(
+        patrimonio,
+        marca,
+        modelo,
+        numero_serie,
+        processador,
+        memoria_ram,
+        armazenamento,
+        data_aquisicao,
+        status,
+        id_funcionario
+    )
+    VALUES(
+        patrimonio_p,
+        marca_p,
+        modelo_p,
+        numero_serie_p,
+        processador_p,
+        memoria_p,
+        armazenamento_p,
+        data_p,
+        status_p,
+        id_funcionario_p
+    );
+ 
+END//
+ 
+DELIMITER ;
